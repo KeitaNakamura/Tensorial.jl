db05609e6dbdca3beac363d0ff0469c7dc4ce518
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-01-16 14:26:20 +0900

    Export more types and methods

--- src/Tensorial.jl
+++ src/Tensorial.jl
@@ -1,19 +1,31 @@
 module Tensorial
 
 using LinearAlgebra
+# re-exports from LinearAlgebra
+export ⋅, dot
+
 using StaticArrays
 using Base: @pure, @_inline_meta
 
 import LinearAlgebra: dot
 
 export
+# Types
     Symmetry,
-    @Symmetry
+    @Symmetry,
+    Tensor,
+# operations
+    contract,
+    ⊗,
+    ⊡
 
 include("utils.jl")
 include("symmetry.jl")
 include("indexing.jl")
 include("tensor.jl")
 include("ops.jl")
+
+const ⊗ = otimes
+const ⊡ = dcontract
 
 end # module
