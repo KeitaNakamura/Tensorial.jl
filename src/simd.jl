a2f99c81bff743d50c93fbb7a5792e41980a216a
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-02-08 21:48:00 +0900

    Implement more efficient simd computations for symmetric tensors

--- src/simd.jl
+++ src/simd.jl
@@ -1,37 +1,37 @@
 const SIMDTypes = Union{Float16, Float32, Float64}
 
-# TODO: implement more efficient computations for symmetric case
-@generated function contraction(x::Tensor{<: Any, T}, y::Tensor{<: Any, T}, ::Val{N}) where {T <: SIMDTypes, N}
+@generated function contraction(x::Tensor{<: Any, T, order1}, y::Tensor{<: Any, T, order2}, ::Val{N}) where {T <: SIMDTypes, N, order1, order2}
     S1 = Space(x)
     S2 = Space(y)
-    S = contraction(S1, S2, Val(N))
-    s1 = map(i -> EinsumIndex(:(Tuple(x)), i), independent_indices(S1))
-    s2 = map(i -> EinsumIndex(:(Tuple(y)), i), independent_indices(S2))
-    J = prod(size(s2)[1:N])
-    I = length(s1) ÷ J
-    K = length(s2) ÷ J
-    s1′ = reshape(s1, I, J)
-    s2′ = reshape(s2, J, K)
-    # Create slices
-    # [a c
-    #  b d] => [[a,b], [c,d]]
-    slices = map(axes(s1′, 2)) do j
-        :($(Symbol(:v, j)) = SIMD.Vec($(map(construct_expr, s1′[:,j])...)))
+    S_Inner = Space((Dims(S2)[i] for i in 1:N)...)
+    S1 = otimes(droplast(S1, Val(N)), S_Inner)
+    S2 = otimes(S_Inner, dropfirst(S2, Val(N)))
+    s1 = [:(Tuple(x)[$i]) for i in 1:ncomponents(S1)]
+    s2 = [:(Tuple(y)[$i]) for i in 1:ncomponents(S2)]
+    K = prod(Dims(S_Inner))
+    I = length(s1) ÷ K
+    J = length(s2) ÷ K
+    s1′ = reshape(s1, I, K)
+    s2′ = reshape(s2, K, J)
+    # column slices of s1′
+    slices = map(1:K) do k
+        :($(Symbol(:v, k)) = SIMD.Vec($(s1′[:,k]...)))
     end
-    columns = map(axes(s2′, 2)) do j
-        coefs = map(axes(s2′, 1)) do i
-            construct_expr(s2′[i, j])
-        end
-        code = :(v1 * $(coefs[1]))
+    # parallel computations along with columns
+    columns = map(1:J) do j
+        coefs = [s2′[k, j] for k in 1:K]
+        ex = :(v1 * $(coefs[1]))
         for i in 2:length(coefs)
-            code = :(muladd($(Symbol(:v, i)), $(coefs[i]), $code))
+            ex = :(muladd($(Symbol(:v, i)), $(coefs[i]), $ex))
         end
-        code
+        ex
     end
-    exps = map(indices(S)) do i
-        d, r = divrem(i-1, I) .+ 1
-        :(columns[$d][$r])
+    # flatten data
+    exps = Expr[]
+    for j in 1:J, i in 1:I
+        push!(exps, :($(columns[j])[$i]))
     end
+    S = contraction(S1, S2, Val(N))
     if length(S) == 0
         TT = T
     else
@@ -39,6 +39,8 @@ const SIMDTypes = Union{Float16, Float32, Float64}
     end
     quote
         @_inline_meta
+        x = convert($(tensortype(S1)){T}, x)
+        y = convert($(tensortype(S2)){T}, y)
         @inbounds begin
             $(slices...)
             columns = tuple($(columns...))
