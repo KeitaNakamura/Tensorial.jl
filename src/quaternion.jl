38dde2dadc8df6c6a84c5059fdb570cced513539
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-12 11:07:40 +0900

    Revert "Change the order of elements of quaternion"
    This reverts commit 82eb6a8bb25722e58e7c117743e59bcbda41085a.

--- src/quaternion.jl
+++ src/quaternion.jl
@@ -8,44 +8,33 @@ end
 
 @inline Quaternion(data::NTuple{4, Any}) = Quaternion{promote_ntuple_eltype(data)}(data)
 @inline (::Type{T})(data::Vararg{Any}) where {T <: Quaternion} = T(data)
-
-# Quaternion <-> Vec
 @inline Quaternion(v::Vec{4}) = Quaternion(Tuple(v))
 @inline Vec(q::Quaternion) = Vec(Tuple(q))
 
 Base.Tuple(q::Quaternion) = getfield(q, :data)
 
 @inline function Base.getproperty(q::Quaternion, name::Symbol)
-    name == :vector && return @inbounds Vec(q[1], q[2], q[3])
-    name == :scalar && return @inbounds q[4]
+    name == :scalar && return @inbounds q[1]
+    name == :vector && return @inbounds Vec(q[2], q[3], q[4])
     getfield(q, name)
 end
 
 Base.propertynames(q::Quaternion) = (:scalar, :vector, :data)
 
 """
-    quaternion(v::Vec, θ; [degree = false])
+    quaternion(x, θ; [degree = false])
 
-Construct `Quaternion` from direction `v` and angle `θ`.
+Construct `Quaternion` from direction `x` and angle `θ`.
 The constructed quaternion is normalized such as `norm(q) ≈ 1`.
-
-```jldoctest
-julia> quaternion(Vec(1.0,2.0,3.0), π/2)
-```
 """
-function quaternion(v::Vec{3}, θ::Real; degree::Bool = false)
+function quaternion(x::Vec{3}, θ::Real; degree::Bool = false)
     if degree
         θ = deg2rad(θ)
     end
     ϕ = θ / 2
     sinϕ = sin(ϕ)
-    n = normalize(v) * sinϕ
-    T = promote_type(typeof(ϕ), eltype(n))
-    @inbounds Quaternion{T}(n[1], n[2], n[3], cos(ϕ))
-end
-
-function Base.summary(io::IO, q::Quaternion)
-    print(io, "Quaternion{", eltype(q), "}<q₁i+q₂j+q₃k+q₄>")
+    n = normalize(x) * sinϕ
+    @inbounds Quaternion(cos(ϕ), n[1], n[2], n[3])
 end
 
 Base.size(::Quaternion) = (4,)
@@ -64,28 +53,29 @@ end
 
 @inline function Base.:*(q::Quaternion, p::Quaternion)
     q1, q2, q3, q4 = Tuple(q)
-    A = @Mat [ q4 -q3  q2  q1
-               q3  q4 -q1  q2
-              -q2  q1  q4  q3
-              -q1 -q2 -q3  q4 ]
+    A = @Mat [ q1 -q2 -q3 -q4
+               q2  q1 -q4  q3
+               q3  q4  q1 -q2
+               q4 -q3  q2  q1 ]
     Quaternion(A ⋅ Vec(p))
 end
 
 @inline function Base.:*(q::Quaternion, v::Vec{3, T}) where {T}
-    @inbounds q * Quaternion(v[1], v[2], v[3], zero(T))
+    @inbounds q * Quaternion(zero(T), v[1], v[2], v[3])
 end
 @inline function Base.:*(v::Vec{3, T}, q::Quaternion) where {T}
-    @inbounds Quaternion(v[1], v[2], v[3], zero(T)) * q
+    @inbounds Quaternion(zero(T), v[1], v[2], v[3]) * q
 end
 
 @inline norm(q::Quaternion) = norm(Vec(q))
 
 @inline function inv(q::Quaternion)
     qvec = Vec(q)
-    @inbounds Quaternion(-q[1], -q[2], -q[3], q[4]) / dot(qvec, qvec)
+    @inbounds Quaternion(q[1], -q[2], -q[3], -q[4]) / dot(qvec, qvec)
 end
 
 function rotmat(q::Quaternion)
+    s = 1 / norm(q)
     q1, q2, q3, q4 = Tuple(q)
     q1² = q1 * q1
     q2² = q2 * q2
@@ -97,7 +87,7 @@ function rotmat(q::Quaternion)
     q1q3 = q1 * q3
     q1q4 = q1 * q4
     q2q4 = q2 * q4
-    @Mat [q1²-q2²-q3²+q4²  2(q1q2-q3q4)     2(q1q3+q2q4)
-          2(q1q2+q3q4)     -q1²+q2²-q3²+q4² 2(q2q3-q1q4)
-          2(q1q3-q2q4)     2(q2q3+q1q4)     -q1²-q2²+q3²+q4²]
+    @Mat [1-2s*(q3²+q4²) 2(q2q3-q1q4)   2(q2q4+q1q3)
+          2(q2q3+q1q4)   1-2s*(q2²+q4²) 2(q3q4-q1q2)
+          2(q2q4-q1q3)   2(q3q4+q1q2)   1-2s*(q2²+q3²)]
 end
