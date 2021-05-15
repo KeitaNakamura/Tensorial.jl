b060b458f29f7fd67691592160934f8f4cf9b846
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-05-15 14:00:53 +0900

    Modify tests

--- test/Tensor.jl
+++ test/Tensor.jl
@@ -253,35 +253,6 @@ end
     @test (@inferred size(TT))::Tuple{Int, Int, Int, Int} == (2,3,3,3)
 end
 
-@testset "AbstractTensor" begin
-    struct Point{dim, T} <: AbstractVec{dim, T}
-        x::Vec{dim, T}
-    end
-    Base.Tuple(p::Point) = Tuple(p.x)
-    Base.getindex(p::Point, i::Int) = p.x[i]
-    for T in (Float32, Float64)
-        x = Vec{2, T}(1, 2)
-        p = Point(x)
-        @test (@inferred p + p)::Vec{2, T} |> Tuple == (x + x).data
-        @test (@inferred p - p)::Vec{2, T} |> Tuple == (x - x).data
-        @test (@inferred p ⋅ p)::T == x ⋅ x
-        @test (@inferred p ⊗ p)::Mat{2, 2, T} == x ⊗ x
-    end
-
-    # size/axes
-    TT = typeof(rand(Mat{3, 2}))
-    @test (@inferred size(TT)) == (3, 2)
-    @test (@inferred size(TT, 1)) == 3
-    @test (@inferred size(TT, 2)) == 2
-    @test (@inferred size(TT, 3)) == 1
-    @test (@inferred size(TT, 4)) == 1
-    @test (@inferred axes(TT)) == (Base.OneTo(3), Base.OneTo(2))
-    @test (@inferred axes(TT, 1)) == Base.OneTo(3)
-    @test (@inferred axes(TT, 2)) == Base.OneTo(2)
-    @test (@inferred axes(TT, 3)) == Base.OneTo(1)
-    @test (@inferred axes(TT, 4)) == Base.OneTo(1)
-end
-
 @testset "Promotion" begin
     x = Vec(1,2,3)
     for T in (Float32, Float64)
