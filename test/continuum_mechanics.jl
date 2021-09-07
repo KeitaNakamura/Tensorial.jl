1d3d2b4c56d8a50f53095ae74e3a87d6ea23682f
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-09-07 11:07:22 +0900

    Implement vol/dev fourth order tensors (#122)

--- test/continuum_mechanics.jl
+++ test/continuum_mechanics.jl
@@ -6,11 +6,27 @@
             # mean
             @test (@inferred mean(x))::T ≈ tr(Array(x)) / 3
             @test (@inferred mean(y))::T ≈ tr(Array(y)) / 3
-            # vol/dev
+            # vol/dev for 2nd-order tensors
             @test (@inferred vol(x))::typeof(x) + (@inferred dev(x))::typeof(x) ≈ x
             @test (@inferred vol(y))::typeof(y) + (@inferred dev(y))::typeof(y) ≈ y
             @test (@inferred vol(x))::typeof(x) ⊡ (@inferred dev(x))::typeof(x) ≈ zero(T)  atol = sqrt(eps(T))
             @test (@inferred vol(y))::typeof(y) ⊡ (@inferred dev(y))::typeof(y) ≈ zero(T)  atol = sqrt(eps(T))
+
+            # vol/dev for 4th-order tensors
+            δ = one(SymmetricSecondOrderTensor{3, T})
+            I_vol = @einsum (i,j,k,l) -> δ[i,j]*δ[k,l]/3
+            I_dev = @einsum (i,j,k,l) -> δ[i,k]*δ[j,l] - δ[i,j]*δ[k,l]/3
+            Isym_dev = @einsum (i,j,k,l) -> (δ[i,k]*δ[j,l]+δ[i,l]*δ[j,k])/2 - δ[i,j]*δ[k,l]/3
+            @test (@inferred vol(FourthOrderTensor{3}))::FourthOrderTensor{3, Float64}                   ≈ I_vol
+            @test (@inferred dev(FourthOrderTensor{3}))::FourthOrderTensor{3, Float64}                   ≈ I_dev
+            @test (@inferred vol(FourthOrderTensor{3, T}))::FourthOrderTensor{3, T}                      ≈ I_vol
+            @test (@inferred dev(FourthOrderTensor{3, T}))::FourthOrderTensor{3, T}                      ≈ I_dev
+            @test (@inferred vol(SymmetricFourthOrderTensor{3}))::SymmetricFourthOrderTensor{3, Float64} ≈ I_vol
+            @test (@inferred dev(SymmetricFourthOrderTensor{3}))::SymmetricFourthOrderTensor{3, Float64} ≈ Isym_dev
+            @test (@inferred vol(SymmetricFourthOrderTensor{3, T}))::SymmetricFourthOrderTensor{3, T}    ≈ I_vol
+            @test (@inferred dev(SymmetricFourthOrderTensor{3, T}))::SymmetricFourthOrderTensor{3, T}    ≈ Isym_dev
+            @test (vol(FourthOrderTensor{3}) + dev(FourthOrderTensor{3}))::FourthOrderTensor{3} ≈ one(FourthOrderTensor{3})
+            @test (vol(SymmetricFourthOrderTensor{3}) + dev(SymmetricFourthOrderTensor{3}))::SymmetricFourthOrderTensor{3} ≈ one(SymmetricFourthOrderTensor{3})
         end
     end
     @testset "stress invariants" begin
