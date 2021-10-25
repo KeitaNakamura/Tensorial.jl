cce423c30ab57c3ed3a5f03b499dc53b66aa23fd
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-10-25 18:56:54 +0900

    Use `Ref` to run benchmarks (#130)
    * Use `Ref` to run benchmarks
    
    * Update benchmarks in README.md

--- benchmark/ops.jl
+++ benchmark/ops.jl
@@ -15,58 +15,58 @@ for T in (Float64, #=Float32=#)
     end
 
     # dot
-    suite["Tensor"]["Space(3) ⋅ Space(3)"] = @benchmarkable $A3 ⋅ $A3
-    suite["Tensor"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $A3x3 ⋅ $A3
-    suite["Tensor"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $A3x3s ⋅ $A3
+    suite["Tensor"]["Space(3) ⋅ Space(3)"] = @benchmarkable $(Ref(A3))[] ⋅ $(Ref(A3))[]
+    suite["Tensor"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $(Ref(A3x3))[] ⋅ $(Ref(A3))[]
+    suite["Tensor"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $(Ref(A3x3s)[]) ⋅ $(Ref(A3))[]
     if run_array
-        suite["Array"]["Space(3) ⋅ Space(3)"] = @benchmarkable $B3 ⋅ $B3
-        suite["Array"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $B3x3 * $B3
-        suite["Array"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $B3x3s * $B3
+        suite["Array"]["Space(3) ⋅ Space(3)"] = @benchmarkable $(Ref(B3))[] ⋅ $(Ref(B3))[]
+        suite["Array"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $(Ref(B3x3))[] * $(Ref(B3))[]
+        suite["Array"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $(Ref(B3x3s))[] * $(Ref(B3))[]
     end
 
     # double_contraction
-    suite["Tensor"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $A3x3 ⊡ $A3x3
-    suite["Tensor"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $A3x3s ⊡ $A3x3s
-    suite["Tensor"]["Space(3,3,3) ⊡ Space(3,3)"] = @benchmarkable $A3x3x3 ⊡ $A3x3
-    suite["Tensor"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $A3x3x3x3 ⊡ $A3x3
-    suite["Tensor"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $A3x3x3x3s ⊡ $A3x3s
+    suite["Tensor"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(A3x3))[] ⊡ $(Ref(A3x3))[]
+    suite["Tensor"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(A3x3s))[] ⊡ $(Ref(A3x3s))[]
+    suite["Tensor"]["Space(3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(A3x3x3))[] ⊡ $(Ref(A3x3))[]
+    suite["Tensor"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(A3x3x3x3))[] ⊡ $(Ref(A3x3))[]
+    suite["Tensor"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(A3x3x3x3s))[] ⊡ $(Ref(A3x3s))[]
     if run_array
-        suite["Array"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $B3x3 ⋅ $B3x3
-        suite["Array"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $B3x3s ⋅ $B3x3s
-        suite["Array"]["Space(3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(reshape(B3x3x3, 3, 9)) * $(reshape(B3x3, 9, 1))
-        suite["Array"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(reshape(B3x3x3x3, 9, 9)) * $(reshape(B3x3, 9, 1))
-        suite["Array"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(reshape(B3x3x3x3s, 9, 9)) * $(reshape(B3x3s, 9, 1))
+        suite["Array"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(B3x3))[] ⋅ $(Ref(B3x3))[]
+        suite["Array"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(B3x3s))[] ⋅ $(Ref(B3x3s))[]
+        suite["Array"]["Space(3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(reshape(B3x3x3, 3, 9)))[] * $(Ref(reshape(B3x3, 9, 1)))[]
+        suite["Array"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(reshape(B3x3x3x3, 9, 9)))[] * $(Ref(reshape(B3x3, 9, 1)))[]
+        suite["Array"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(reshape(B3x3x3x3s, 9, 9)))[] * $(Ref(reshape(B3x3s, 9, 1)))[]
     end
 
     # otimes
-    suite["Tensor"]["Space(3) ⊗ Space(3)"] = @benchmarkable $A3 ⊗ $A3
+    suite["Tensor"]["Space(3) ⊗ Space(3)"] = @benchmarkable $(Ref(A3))[] ⊗ $(Ref(A3))[]
     if run_array
-        suite["Array"]["Space(3) ⊗ Space(3)"] = @benchmarkable $B3 * $B3'
+        suite["Array"]["Space(3) ⊗ Space(3)"] = @benchmarkable $(Ref(B3))[] * $(Ref(B3))[]'
     end
 
     # cross
-    suite["Tensor"]["Space(3) × Space(3)"] = @benchmarkable $A3 × $A3
+    suite["Tensor"]["Space(3) × Space(3)"] = @benchmarkable $(Ref(A3))[] × $(Ref(A3))[]
     if run_array
-        suite["Array"]["Space(3) × Space(3)"] = @benchmarkable $B3 × $B3
+        suite["Array"]["Space(3) × Space(3)"] = @benchmarkable $(Ref(B3))[] × $(Ref(B3))[]
     end
 
     # det
-    suite["Tensor"]["det(Space(3,3))"] = @benchmarkable det($A3x3)
-    suite["Tensor"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($A3x3s)
+    suite["Tensor"]["det(Space(3,3))"] = @benchmarkable det($(Ref(A3x3))[])
+    suite["Tensor"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($(Ref(A3x3s))[])
     if run_array
-        suite["Array"]["det(Space(3,3))"] = @benchmarkable det($B3x3)
-        suite["Array"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($B3x3s)
+        suite["Array"]["det(Space(3,3))"] = @benchmarkable det($(Ref(B3x3))[])
+        suite["Array"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($(Ref(B3x3s))[])
     end
 
     # inv
-    suite["Tensor"]["inv(Space(3,3))"] = @benchmarkable inv($A3x3)
-    suite["Tensor"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($A3x3s)
-    suite["Tensor"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($A3x3x3x3)
-    suite["Tensor"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($A3x3x3x3s)
+    suite["Tensor"]["inv(Space(3,3))"] = @benchmarkable inv($(Ref(A3x3))[])
+    suite["Tensor"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($(Ref(A3x3s))[])
+    suite["Tensor"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(Ref(A3x3x3x3))[])
+    suite["Tensor"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(Ref(A3x3x3x3s))[])
     if run_array
-        suite["Array"]["inv(Space(3,3))"] = @benchmarkable inv($B3x3)
-        suite["Array"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($B3x3s)
-        suite["Array"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(reshape(B3x3x3x3, 9, 9)))
-        suite["Array"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(reshape(B3x3x3x3, 9, 9))) # B3x3x3x3s is not invertible
+        suite["Array"]["inv(Space(3,3))"] = @benchmarkable inv($(Ref(B3x3))[])
+        suite["Array"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($(Ref(B3x3s))[])
+        suite["Array"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(Ref(reshape(B3x3x3x3, 9, 9)))[])
+        suite["Array"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(Ref(reshape(B3x3x3x3, 9, 9)))[]) # B3x3x3x3s is not invertible
     end
 end
