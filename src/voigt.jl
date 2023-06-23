ff99f227accc6902009057f26f64cf422ceb673b
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-06-23 11:22:47 +0900

    Add documentation for gradient and hessian, do not specialize Tag in dualize (#185)
    * Don't specialize Tag in dualize
    
    * Add documentation for gradient and hessian
    
    * Fix docs
    
    * Set version to 0.12.16

--- src/voigt.jl
+++ src/voigt.jl
@@ -27,7 +27,7 @@ julia> x = Mat{3,3}(1:9...)
  3  6  9
 
 julia> tovoigt(x)
-9-element StaticArrays.SVector{9, Int64} with indices SOneTo(9):
+9-element StaticArraysCore.SVector{9, Int64} with indices SOneTo(9):
  1
  5
  9
@@ -46,7 +46,7 @@ julia> x = SymmetricSecondOrderTensor{3}(1:6...)
 
 julia> tovoigt(x; offdiagscale = 2,
                   order = [(1,1), (2,2), (3,3), (1,2), (1,3), (2,3)])
-6-element StaticArrays.SVector{6, Int64} with indices SOneTo(6):
+6-element StaticArraysCore.SVector{6, Int64} with indices SOneTo(6):
   1
   4
   6
