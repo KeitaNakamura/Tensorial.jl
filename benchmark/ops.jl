853df44af9996f185d0753a56fbde22143be5732
Keita Nakamura <keita.nakamura.1109@gmail.com>
2024-10-20 09:42:10 +0900

    Update benchmarks

--- benchmark/ops.jl
+++ benchmark/ops.jl
@@ -2,71 +2,79 @@ for T in (Float64, #=Float32=#)
     A3 = tensors[(Space(3), T)]
     A3x3 = tensors[(Space(3,3), T)]
     A3x3s = tensors[(Space(Symmetry(3,3)), T)]
-    A3x3x3 = tensors[(Space(3,3,3), T)]
     A3x3x3x3 = tensors[(Space(3,3,3,3), T)]
     A3x3x3x3s = tensors[(Space(Symmetry(3,3),Symmetry(3,3)), T)]
-    if run_array
-        B3 = Array(A3)
-        B3x3 = Array(A3x3)
-        B3x3s = Array(A3x3s)
-        B3x3x3 = Array(A3x3x3)
-        B3x3x3x3 = Array(A3x3x3x3)
-        B3x3x3x3s = Array(A3x3x3x3s)
-    end
+    # Array
+    B3 = Array(A3)
+    B3x3 = Array(A3x3)
+    B3x3s = Array(A3x3s)
+    B3x3_voigt = Array(tovoigt(A3x3))
+    B3x3s_voigt = Array(tovoigt(A3x3s))
+    B3x3x3x3_voigt = Array(tovoigt(A3x3x3x3))
+    B3x3x3x3s_voigt = Array(tovoigt(A3x3x3x3s))
+    # SArray
+    C3 = SArray(A3)
+    C3x3 = SArray(A3x3)
+    C3x3s = SArray(A3x3s)
+    C3x3_voigt = tovoigt(A3x3)
+    C3x3s_voigt = tovoigt(A3x3s)
+    C3x3x3x3_voigt = tovoigt(A3x3x3x3)
+    C3x3x3x3s_voigt = tovoigt(A3x3x3x3s)
 
     # dot
     suite["Tensor"]["Space(3) ⋅ Space(3)"] = @benchmarkable $(Ref(A3))[] ⋅ $(Ref(A3))[]
     suite["Tensor"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $(Ref(A3x3))[] ⋅ $(Ref(A3))[]
     suite["Tensor"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $(Ref(A3x3s)[]) ⋅ $(Ref(A3))[]
-    if run_array
-        suite["Array"]["Space(3) ⋅ Space(3)"] = @benchmarkable $(Ref(B3))[] ⋅ $(Ref(B3))[]
-        suite["Array"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $(Ref(B3x3))[] * $(Ref(B3))[]
-        suite["Array"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $(Ref(B3x3s))[] * $(Ref(B3))[]
-    end
+    suite["Array"]["Space(3) ⋅ Space(3)"] = @benchmarkable $(Ref(B3))[] ⋅ $(Ref(B3))[]
+    suite["Array"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $(Ref(B3x3))[] * $(Ref(B3))[]
+    suite["Array"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $(Ref(B3x3s))[] * $(Ref(B3))[]
+    suite["SArray"]["Space(3) ⋅ Space(3)"] = @benchmarkable $(Ref(C3))[] ⋅ $(Ref(C3))[]
+    suite["SArray"]["Space(3,3) ⋅ Space(3)"] = @benchmarkable $(Ref(C3x3))[] * $(Ref(C3))[]
+    suite["SArray"]["Space(Symmetry(3,3)) ⋅ Space(3)"] = @benchmarkable $(Ref(C3x3s))[] * $(Ref(C3))[]
 
     # double_contraction
     suite["Tensor"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(A3x3))[] ⊡ $(Ref(A3x3))[]
     suite["Tensor"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(A3x3s))[] ⊡ $(Ref(A3x3s))[]
-    suite["Tensor"]["Space(3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(A3x3x3))[] ⊡ $(Ref(A3x3))[]
     suite["Tensor"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(A3x3x3x3))[] ⊡ $(Ref(A3x3))[]
     suite["Tensor"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(A3x3x3x3s))[] ⊡ $(Ref(A3x3s))[]
-    if run_array
-        suite["Array"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(B3x3))[] ⋅ $(Ref(B3x3))[]
-        suite["Array"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(B3x3s))[] ⋅ $(Ref(B3x3s))[]
-        suite["Array"]["Space(3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(reshape(B3x3x3, 3, 9)))[] * $(Ref(reshape(B3x3, 9, 1)))[]
-        suite["Array"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(reshape(B3x3x3x3, 9, 9)))[] * $(Ref(reshape(B3x3, 9, 1)))[]
-        suite["Array"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(reshape(B3x3x3x3s, 9, 9)))[] * $(Ref(reshape(B3x3s, 9, 1)))[]
-    end
+    suite["Array"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(B3x3_voigt))[] ⋅ $(Ref(B3x3_voigt))[]
+    suite["Array"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(B3x3s_voigt))[] ⋅ $(Ref(B3x3s_voigt))[]
+    suite["Array"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(B3x3x3x3_voigt))[] * $(Ref(B3x3_voigt))[]
+    suite["Array"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(B3x3x3x3s_voigt))[] * $(Ref(B3x3s_voigt))[]
+    suite["SArray"]["Space(3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(C3x3))[] ⋅ $(Ref(C3x3))[]
+    suite["SArray"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(C3x3s))[] ⋅ $(Ref(C3x3s))[]
+    suite["SArray"]["Space(3,3,3,3) ⊡ Space(3,3)"] = @benchmarkable $(Ref(C3x3x3x3_voigt))[] * $(Ref(C3x3_voigt))[]
+    suite["SArray"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"] = @benchmarkable $(Ref(C3x3x3x3s_voigt))[] * $(Ref(C3x3s_voigt))[]
 
     # otimes
     suite["Tensor"]["Space(3) ⊗ Space(3)"] = @benchmarkable $(Ref(A3))[] ⊗ $(Ref(A3))[]
-    if run_array
-        suite["Array"]["Space(3) ⊗ Space(3)"] = @benchmarkable $(Ref(B3))[] * $(Ref(B3))[]'
-    end
+    suite["Array"]["Space(3) ⊗ Space(3)"] = @benchmarkable $(Ref(B3))[] * $(Ref(B3))[]'
+    suite["SArray"]["Space(3) ⊗ Space(3)"] = @benchmarkable $(Ref(C3))[] * $(Ref(C3))[]'
 
     # cross
     suite["Tensor"]["Space(3) × Space(3)"] = @benchmarkable $(Ref(A3))[] × $(Ref(A3))[]
-    if run_array
-        suite["Array"]["Space(3) × Space(3)"] = @benchmarkable $(Ref(B3))[] × $(Ref(B3))[]
-    end
+    suite["Array"]["Space(3) × Space(3)"] = @benchmarkable $(Ref(B3))[] × $(Ref(B3))[]
+    suite["SArray"]["Space(3) × Space(3)"] = @benchmarkable $(Ref(C3))[] × $(Ref(C3))[]
 
     # det
     suite["Tensor"]["det(Space(3,3))"] = @benchmarkable det($(Ref(A3x3))[])
     suite["Tensor"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($(Ref(A3x3s))[])
-    if run_array
-        suite["Array"]["det(Space(3,3))"] = @benchmarkable det($(Ref(B3x3))[])
-        suite["Array"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($(Ref(B3x3s))[])
-    end
+    suite["Array"]["det(Space(3,3))"] = @benchmarkable det($(Ref(B3x3))[])
+    suite["Array"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($(Ref(B3x3s))[])
+    suite["SArray"]["det(Space(3,3))"] = @benchmarkable det($(Ref(C3x3))[])
+    suite["SArray"]["det(Space(Symmetry(3,3)))"] = @benchmarkable det($(Ref(C3x3s))[])
 
     # inv
     suite["Tensor"]["inv(Space(3,3))"] = @benchmarkable inv($(Ref(A3x3))[])
     suite["Tensor"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($(Ref(A3x3s))[])
     suite["Tensor"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(Ref(A3x3x3x3))[])
     suite["Tensor"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(Ref(A3x3x3x3s))[])
-    if run_array
-        suite["Array"]["inv(Space(3,3))"] = @benchmarkable inv($(Ref(B3x3))[])
-        suite["Array"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($(Ref(B3x3s))[])
-        suite["Array"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(Ref(reshape(B3x3x3x3, 9, 9)))[])
-        suite["Array"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(Ref(reshape(B3x3x3x3, 9, 9)))[]) # B3x3x3x3s is not invertible
-    end
+    suite["Array"]["inv(Space(3,3))"] = @benchmarkable inv($(Ref(B3x3))[])
+    suite["Array"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($(Ref(B3x3s))[])
+    suite["Array"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(Ref(B3x3x3x3_voigt))[])
+    suite["Array"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(Ref(B3x3x3x3s_voigt))[])
+    suite["SArray"]["inv(Space(3,3))"] = @benchmarkable inv($(Ref(C3x3))[])
+    suite["SArray"]["inv(Space(Symmetry(3,3)))"] = @benchmarkable inv($(Ref(C3x3s))[])
+    suite["SArray"]["inv(Space(3,3,3,3))"] = @benchmarkable inv($(Ref(C3x3x3x3_voigt))[])
+    suite["SArray"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"] = @benchmarkable inv($(Ref(C3x3x3x3s_voigt))[])
 end
--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -5,8 +5,6 @@ using Markdown
 
 using Tensorial: Space
 
-const run_array = true
-
 const suite = BenchmarkGroup()
 suite["Array"] = BenchmarkGroup()
 suite["Tensor"] = BenchmarkGroup()
@@ -41,72 +39,89 @@ let path = "../docs/src/Benchmarks.md"
         function printheader(head)
             println(file, "| **$(head)** | | | |")
         end
-        function printrow(op, tt, ta)
+        function printrow(op, tt, ta, ts)
             pretty = (t) -> BenchmarkTools.prettytime(BenchmarkTools.time(minimum(t)))
             speedup = (ta, tt) -> round(10*BenchmarkTools.time(minimum(ta))/BenchmarkTools.time(minimum(tt)))/10
-            println(file, "| `$(op)` | $(pretty(tt)) | $(pretty(ta)) | ×$(speedup(ta, tt)) |")
+            println(file, "| `$(op)` | $(pretty(tt)) | $(pretty(ta)) | ×$(speedup(ta, tt)) | $(pretty(ts)) | ×$(speedup(ts, tt))")
         end
 
         print(file,
               """
               # Benchmarks
 
+              The performance for some typical operators is summarized below.
+              For fourth-order tensors, both `Array` and `SArray` use the classical [Voigt form](https://en.wikipedia.org/wiki/Voigt_notation)
+              to correctly handle symmetries.
+              The benchmakrs show that `Tensor` offers performance comparable to `SArray` without the hassle of using the Voigt form.
+
               ```julia
               a = rand(Vec{3})
               A = rand(SecondOrderTensor{3})
               S = rand(SymmetricSecondOrderTensor{3})
-              B = rand(Tensor{Tuple{3,3,3}})
               AA = rand(FourthOrderTensor{3})
               SS = rand(SymmetricFourthOrderTensor{3})
               ```
 
-              | Operation  | `Tensor` | `Array` | speed-up |
-              |:-----------|---------:|--------:|---------:|
+              | Operation  | `Tensor` | `Array` | Speedup | `SArray` | Speedup |
+              |:-----------|---------:|--------:|--------:|---------:|--------:|
               """)
 
         printheader("Single contraction")
         printrow("a ⋅ a", results["Tensor"]["Space(3) ⋅ Space(3)"],
-                          results["Array" ]["Space(3) ⋅ Space(3)"])
+                          results["Array" ]["Space(3) ⋅ Space(3)"],
+                          results["SArray"]["Space(3) ⋅ Space(3)"])
         printrow("A ⋅ a", results["Tensor"]["Space(3,3) ⋅ Space(3)"],
-                          results["Array" ]["Space(3,3) ⋅ Space(3)"])
+                          results["Array" ]["Space(3,3) ⋅ Space(3)"],
+                          results["SArray"]["Space(3,3) ⋅ Space(3)"])
         printrow("S ⋅ a", results["Tensor"]["Space(Symmetry(3,3)) ⋅ Space(3)"],
-                          results["Array" ]["Space(Symmetry(3,3)) ⋅ Space(3)"])
+                          results["Array" ]["Space(Symmetry(3,3)) ⋅ Space(3)"],
+                          results["SArray"]["Space(Symmetry(3,3)) ⋅ Space(3)"])
 
         printheader("Double contraction")
         printrow("A ⊡ A", results["Tensor"]["Space(3,3) ⊡ Space(3,3)"],
-                          results["Array" ]["Space(3,3) ⊡ Space(3,3)"])
+                          results["Array" ]["Space(3,3) ⊡ Space(3,3)"],
+                          results["SArray"]["Space(3,3) ⊡ Space(3,3)"])
         printrow("S ⊡ S", results["Tensor"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"],
-                          results["Array" ]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"])
-        printrow("B ⊡ A", results["Tensor"]["Space(3,3,3) ⊡ Space(3,3)"],
-                          results["Array" ]["Space(3,3,3) ⊡ Space(3,3)"])
+                          results["Array" ]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"],
+                          results["SArray"]["Space(Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"])
         printrow("AA ⊡ A", results["Tensor"]["Space(3,3,3,3) ⊡ Space(3,3)"],
-                           results["Array" ]["Space(3,3,3,3) ⊡ Space(3,3)"])
+                           results["Array" ]["Space(3,3,3,3) ⊡ Space(3,3)"],
+                           results["SArray"]["Space(3,3,3,3) ⊡ Space(3,3)"])
         printrow("SS ⊡ S", results["Tensor"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"],
-                           results["Array" ]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"])
+                           results["Array" ]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"],
+                           results["SArray"]["Space(Symmetry(3,3),Symmetry(3,3)) ⊡ Space(Symmetry(3,3))"])
 
         printheader("Tensor product")
         printrow("a ⊗ a", results["Tensor"]["Space(3) ⊗ Space(3)"],
-                          results["Array" ]["Space(3) ⊗ Space(3)"])
+                          results["Array" ]["Space(3) ⊗ Space(3)"],
+                          results["SArray"]["Space(3) ⊗ Space(3)"])
 
         printheader("Cross product")
         printrow("a × a", results["Tensor"]["Space(3) ⊗ Space(3)"],
-                          results["Array" ]["Space(3) ⊗ Space(3)"])
+                          results["Array" ]["Space(3) ⊗ Space(3)"],
+                          results["SArray"]["Space(3) ⊗ Space(3)"])
 
         printheader("Determinant")
         printrow("det(A)", results["Tensor"]["det(Space(3,3))"],
-                           results["Array" ]["det(Space(3,3))"])
+                           results["Array" ]["det(Space(3,3))"],
+                           results["SArray"]["det(Space(3,3))"])
         printrow("det(S)", results["Tensor"]["det(Space(Symmetry(3,3)))"],
-                           results["Array" ]["det(Space(Symmetry(3,3)))"])
+                           results["Array" ]["det(Space(Symmetry(3,3)))"],
+                           results["SArray"]["det(Space(Symmetry(3,3)))"])
 
         printheader("Inverse")
         printrow("inv(A)", results["Tensor"]["inv(Space(3,3))"],
-                           results["Array" ]["inv(Space(3,3))"])
+                           results["Array" ]["inv(Space(3,3))"],
+                           results["SArray"]["inv(Space(3,3))"])
         printrow("inv(S)", results["Tensor"]["inv(Space(Symmetry(3,3)))"],
-                           results["Array" ]["inv(Space(Symmetry(3,3)))"])
+                           results["Array" ]["inv(Space(Symmetry(3,3)))"],
+                           results["SArray"]["inv(Space(Symmetry(3,3)))"])
         printrow("inv(AA)", results["Tensor"]["inv(Space(3,3,3,3))"],
-                            results["Array" ]["inv(Space(3,3,3,3))"])
+                            results["Array" ]["inv(Space(3,3,3,3))"],
+                            results["SArray"]["inv(Space(3,3,3,3))"])
         printrow("inv(SS)", results["Tensor"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"],
-                            results["Array" ]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"])
+                            results["Array" ]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"],
+                            results["SArray"]["inv(Space(Symmetry(3,3),Symmetry(3,3)))"])
 
         # print versioninfo
         print(file,
